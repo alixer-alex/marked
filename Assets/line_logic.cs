14c88cf2594ab04516c7e2f3f79b84cdbfa21c5f
alixer-alex <152109823+alixer-alex@users.noreply.github.com>
2026-01-30 01:39:57 -0800

    yes!
    prefab for drawing created
    can draw now
    dope sawg

--- Assets/line_logic.cs
+++ Assets/line_logic.cs
@@ -4,32 +4,87 @@ using UnityEngine;
 
 public class line_logic : MonoBehaviour
 {
+        private List<LineRenderer> lineList;
         private LineRenderer line;
+        private bool isDrawing;
         private Vector3 prevPosition;
-        [SerializeField]
-        private float minDistance=0.1F;
+        private float time;
+        private bool startTimer;
+        [SerializeField] private LineRenderer template;
+        [SerializeField] private float minDistance=0.1F;
     // Start is called before the first frame update
     void Start()
     {
-        line = GetComponent<LineRenderer>();
+        lineList = new List<LineRenderer>();
         prevPosition = transform.position;
+        isDrawing = false;
+        time = 0;
     }
+
+    void StartLine()
+    {
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        currentPosition.z = 0f;
+        line = Instantiate(template, this.transform);
+        lineList.Add(line);
+        line.positionCount = 1;
+        line.SetPosition(0, currentPosition);
+        prevPosition = currentPosition;
+        isDrawing = true;
+        startTimer = false;
+        time = 0;
+
+    }
+    
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0) && !isDrawing)
+        {
+            StartLine();
+
+        }
+
         if(Input.GetMouseButton(0)){
-            Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            currentPosition.z = 0f;
-            if(Vector3.Distance(currentPosition,prevPosition )>minDistance)
+            if (isDrawing)
             {
-                line.positionCount++;
-                line.SetPosition(line.positionCount-1,currentPosition);
-                prevPosition = currentPosition;
+                Vector3 currentPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentPosition.z = 0f;
+                if (Vector3.Distance(currentPosition, prevPosition) > minDistance)
+                {
+                    line.positionCount++;
+                    line.SetPosition(line.positionCount - 1, currentPosition);
+                    prevPosition = currentPosition;
+                }
             }
 
         }
 
-    
+        if (Input.GetMouseButtonUp(0))
+        {
+            line = null;
+            isDrawing = false;
+            startTimer = true;
+            time = 0;
+        }
+
+        if (startTimer)
+        {
+            time+=Time.deltaTime;
+            if (time > 5.0)
+            {
+                //do save
+                
+                foreach (LineRenderer l in lineList)
+                {
+                    Destroy(l.gameObject);
+                    startTimer = false;
+                }
+                
+                lineList = new List<LineRenderer>();
+            }
+        }
+        
     }
 }
